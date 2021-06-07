66aedcf4503b50c0e86c6e67f0e43c00004733ff
israel <ip345622@gmail.com>
2021-06-06 20:32:40 -0500

    primer borrador

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,8 @@
+var mensaje = "Bienvenido a nuestro sitio web";
+
 $(".menu").on('click', function(){
     $("nav ul").toggle('slow');
 })
+
+// a continuación se muestra un mensaje
+alert(mensaje);
