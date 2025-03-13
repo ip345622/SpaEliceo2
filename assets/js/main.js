bb0e248ffd555e64b5c16f542c4cde90cabe6328
Israel <ip345622@gmail.com>
2025-03-13 16:34:15 -0600

    Re Diseño del Spa

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,14 @@
-var mensaje = "Bienvenido a nuestro sitio web";
-
-$(".menu").on('click', function(){
-    $("nav ul").toggle('slow');
-})
-
-// a continuación se muestra un mensaje
-alert(mensaje);
+const menuBtn = document.getElementById("menu-btn");
+        const menu = document.getElementById("menu");
+    
+        menuBtn.addEventListener("click", () => {
+            menu.classList.toggle("hidden");
+            menu.classList.toggle("flex");
+            menu.classList.toggle("flex-col");
+            menu.classList.toggle("absolute");
+            menu.classList.toggle("top-16");
+            menu.classList.toggle("left-0");
+            menu.classList.toggle("w-full");
+            menu.classList.toggle("bg-black");
+            menu.classList.toggle("p-5");
+        });
